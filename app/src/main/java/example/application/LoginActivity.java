36d143bb0ac9ade73b3757f6b74356f9006dcdf1
Dan Tran <autumnssuns@gmail.com>
2023-04-24 11:55:48 +1000

    Updated the LoginActivity to use the Default User

--- app/src/main/java/example/application/LoginActivity.java
+++ app/src/main/java/example/application/LoginActivity.java
@@ -10,9 +10,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import example.data.DefaultUser;
 import example.data.InvalidUserException;
 import example.data.StaticUserDAO;
 import example.data.User;
+
+import java.util.Objects;
 
 /**
  * Represents the login view of the application.
@@ -114,22 +117,23 @@ public class LoginActivity extends AppCompatActivity {
      * @return true if the user was logged in, false otherwise
      */
     private boolean logIn(String email, String password, String name){
-        // Check if the email and password match a registered user.
-        for (User user : staticUserDAO.listUsers()) {
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
-                currentUser = user;
+        try {
+            User matchedUser = staticUserDAO.getUser(email);
+            if (Objects.equals(matchedUser.getPassword(), password)){
+                currentUser = matchedUser;
+            } else {
+                currentUser = new DefaultUser();
             }
-        }
-        // If so, if the name is not empty, update the user's name and log the user in.
-        if (currentUser != null) {
+            // If so, if the name is not empty, update the user's name and log the user in.
             if (!name.isEmpty()) {
                 currentUser.setName(name);
                 staticUserDAO.updateUser(currentUser);
             }
             // Always log the user in.
             return true;
+        } catch (InvalidUserException e) {
+            throw new RuntimeException(e);
         }
-        return false;
     }
 
     /**
@@ -139,13 +143,18 @@ public class LoginActivity extends AppCompatActivity {
      * @return true if the user was logged in, false otherwise
      */
     private boolean logIn(String email, String password) {
-        for (User user : staticUserDAO.listUsers()) {
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
-                currentUser = user;
-                return true;
+        User matchedUser = null;
+        try {
+            matchedUser = staticUserDAO.getUser(email);
+            if (Objects.equals(matchedUser.getPassword(), password)){
+                currentUser = matchedUser;
+            } else {
+                currentUser = new DefaultUser();
             }
+            return true;
+        } catch (InvalidUserException e) {
+            throw new RuntimeException(e);
         }
-        return false;
     }
 
     /**
